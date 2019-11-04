673bd191cca1ed7a5863d3be6501fd19425958e2
martin <martin.sachin@gmail.com>
2019-11-04 22:23:42 +0100

    .gitignore is now working

--- cmake-build-debug/external/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/martin/CLionProjects/RayTracer/external/gtest/googletest
-# Build directory: /home/martin/CLionProjects/RayTracer/cmake-build-debug/external/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
