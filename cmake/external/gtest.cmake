e6a68ade9de5ed01b38722b5195795300bf1d4d1
martin <martin.sachin@gmail.com>
2019-11-27 18:27:57 +0100

    ExternalProject bugfix

--- cmake/external/gtest.cmake
+++ cmake/external/gtest.cmake
@@ -1,9 +1,9 @@
 cmake_minimum_required(VERSION 2.8.2)
-project(googletest-download NONE)
+project(gtest-download NONE)
 
 include(ExternalProject)
 ExternalProject_Add(
-        googletest
+        gtest
         GIT_REPOSITORY https://github.com/google/googletest.git
         GIT_TAG master
         SOURCE_DIR "${EXTERNAL_BINARY_DIR}/gtest-src"
