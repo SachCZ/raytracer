57503f0c0c1f53428cc333428c5fc657d920ed5d
martin <martin.sachin@gmail.com>
2019-12-08 20:11:54 +0100

    Added intersection finding to laserRay.

--- include/raytracer/physics/LaserRay.h
+++ include/raytracer/physics/LaserRay.h
@@ -2,11 +2,14 @@
 #define RAYTRACER_LASER_RAY_H
 
 #include <cmath>
+#include <vector>
 
+#include "Ray.h"
 #include "Magnitudes.h"
 #include "Point.h"
 #include "Vector.h"
 #include "Constants.h"
+#include "GeometryFunctions.h"
 
 namespace raytracer {
     namespace physics {
@@ -21,6 +24,18 @@ namespace raytracer {
             Length wavelength{};
 
             Density getCriticalDensity() const;
+
+            template<typename IntersFunc, typename StopCondition>
+            void generateIntersections(
+                    const geometry::Mesh &mesh,
+                    IntersFunc findInters,
+                    StopCondition stopCondition) {
+                geometry::Ray ray(geometry::HalfLine{this->startPoint, this->direction});
+
+                this->intersections = ray.findIntersections(mesh, findInters, stopCondition);
+            }
+
+            std::vector<geometry::Intersection> intersections;
         };
     }
 }
