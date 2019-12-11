e18ca8d5ed6ccafaa580b849f1b788bb77af40a5
martin <martin.sachin@gmail.com>
2019-12-11 23:16:28 +0100

    Added hack quickfix.

--- src/geometry/MathFunctions.cpp
+++ src/geometry/MathFunctions.cpp
@@ -21,7 +21,7 @@ double raytracer::geometry::impl::getParamT(const HalfLine &halfLine,
 }
 
 bool raytracer::geometry::impl::isIntersecting(double k, double t) {
-    return k > -constants::epsilon && k < 1 + constants::epsilon && t > constants::epsilon;
+    return k > 0 && k < 1 && t > constants::epsilon; //TODO when using the terrible hack, it is possible to compare with 0, 1
 }
 
 std::unique_ptr<raytracer::geometry::Intersection>
@@ -31,9 +31,17 @@ raytracer::geometry::findIntersection(const HalfLine &halfLine,
     if (points.size() == 2){
         double k = impl::getParamK(halfLine, points);
         double t = impl::getParamT(halfLine, points);
+        //TODO delete this terrible hack
+        if (std::abs(k - 1) < constants::epsilon){
+            k = 1 - constants::epsilon;
+        } else if (std::abs(k) < constants::epsilon){
+            k = constants::epsilon;
+        }
 
         if (impl::isIntersecting(k, t)) {
-            auto point = (halfLine.point + t*halfLine.direction);
+            const auto &A = *points[0];
+            const auto &B = *points[1];
+            auto point = (A + k*(B - A));
             auto direction = halfLine.direction;
             HalfLine newHalfLine{point, direction};
             auto intersection = std::make_unique<Intersection>();
