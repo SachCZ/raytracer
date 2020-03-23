a856414c300be203a3adc338cae82cddfd5d753f
martin <martin.sachin@gmail.com>
2020-03-23 16:22:06 +0100

    Split absorption to .cpp

--- include/raytracer/physics/Absorption.h
+++ include/raytracer/physics/Absorption.h
@@ -54,9 +54,7 @@ namespace raytracer {
          * Add an AbsorptionModel that will be used when calling absorb().
          * @param model
          */
-        void addModel(const AbsorptionModel *model) {
-            models.emplace_back(model);
-        }
+        void addModel(const AbsorptionModel *model);
 
         /**
          * Add energy values based on AbsorptionModel applied on the Laser to the absorbedEnergy
@@ -66,35 +64,12 @@ namespace raytracer {
          * @param laser
          * @param absorbedEnergy
          */
-        void absorb(const Laser &laser, MeshFunction &absorbedEnergy) {
-            for (const auto &laserRay : laser.getRays()) {
-                this->absorbLaserRay(laserRay, absorbedEnergy);
-            }
-        }
+        void absorb(const Laser &laser, MeshFunction &absorbedEnergy);
 
     private:
         std::vector<const AbsorptionModel *> models{};
 
-        void absorbLaserRay(const LaserRay &laserRay, MeshFunction &absorbedEnergy) {
-            const auto &intersections = laserRay.intersections;
-            auto intersectionIt = std::next(std::begin(intersections));
-            auto previousIntersectionIt = std::begin(intersections);
-
-            auto currentEnergy = laserRay.energy.asDouble;
-
-            for (; intersectionIt != std::end(intersections); ++intersectionIt, ++previousIntersectionIt) {
-                for (const auto &model : this->models) {
-                    auto absorbed = model->getEnergyChange(
-                            *previousIntersectionIt,
-                            *intersectionIt,
-                            Energy{currentEnergy},
-                            laserRay
-                    ).asDouble;
-                    currentEnergy -= absorbed;
-                    absorbedEnergy.addValue(*(intersectionIt->previousElement), absorbed);
-                }
-            }
-        }
+        void absorbLaserRay(const LaserRay &laserRay, MeshFunction &absorbedEnergy);
     };
     /**
      * @}
