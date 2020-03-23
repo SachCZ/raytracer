7141a71373b7bcd36826b1e707c0428a40a8f007
martin <martin.sachin@gmail.com>
2020-03-23 17:58:26 +0100

    Added bremsstrahlung.

--- include/raytracer/physics/Absorption.h
+++ include/raytracer/physics/Absorption.h
@@ -6,6 +6,7 @@
 #include "Magnitudes.h"
 #include "LaserRay.h"
 #include "Laser.h"
+#include "CollisionalFrequency.h"
 
 namespace raytracer {
     /**
@@ -33,6 +34,68 @@ namespace raytracer {
                 const Energy &currentEnergy,
                 const LaserRay &laserRay
         ) const = 0;
+    };
+
+    /**
+     * Absorption model of energy exchange due to bremsstrahlung.
+     */
+    struct Bremsstrahlung : public AbsorptionModel {
+
+        /**
+         * To be able to model bremsstrahlung, MeshFunction density, temperature, ionization must be provided.
+         * Besides that a collisional frequency model is needed.
+         * @param density
+         * @param temperature
+         * @param ionization
+         * @param collisionalFrequency
+         */
+        explicit Bremsstrahlung(
+                const MeshFunction &density,
+                const MeshFunction &temperature,
+                const MeshFunction &ionization,
+                const CollisionalFrequency &collisionalFrequency
+        ) :
+                _density(density),
+                _temperature(temperature),
+                _ionization(ionization),
+                collisionalFrequency(collisionalFrequency) {}
+
+        /**
+         * Returns the energy absorbed into one element between two intersections based on bremsstrahlung model.
+         * @param previousIntersection
+         * @param currentIntersection
+         * @param currentEnergy
+         * @param laserRay
+         * @return
+         */
+        Energy getEnergyChange(
+                const Intersection &previousIntersection,
+                const Intersection &currentIntersection,
+                const Energy &currentEnergy,
+                const LaserRay &laserRay
+        ) const override {
+            const auto &element = currentIntersection.previousElement;
+            if (!element) return Energy{0};
+            const auto &previousPoint = previousIntersection.pointOnFace.point;
+            const auto &point = currentIntersection.pointOnFace.point;
+
+            const auto distance = (point - previousPoint).getNorm();
+            const auto density = Density{this->_density.getValue(*element)};
+            const auto temperature = Temperature{this->_temperature.getValue(*element)};
+            const auto ionization = this->_ionization.getValue(*element);
+
+            auto frequency = collisionalFrequency.get(density, temperature, laserRay.wavelength, ionization);
+            const auto exponent = -laserRay.getInverseBremsstrahlungCoeff(density, frequency) * distance;
+
+            auto newEnergy = currentEnergy.asDouble * std::exp(exponent);
+            return Energy{currentEnergy.asDouble - newEnergy};
+        }
+
+    private:
+        const MeshFunction &_density;
+        const MeshFunction &_temperature;
+        const MeshFunction &_ionization;
+        const CollisionalFrequency &collisionalFrequency;
     };
 
     /**
