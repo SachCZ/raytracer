697228de374009422001c0655e29a74ba952384d
martin <martin.sachin@gmail.com>
2020-03-27 17:02:05 +0100

    Fixed bug with (0,0) gradient.

--- include/raytracer/physics/Refraction.h
+++ include/raytracer/physics/Refraction.h
@@ -1,70 +1,95 @@
-#include "Propagation.h"
-#include "Refraction.h"
+#ifndef RAYTRACER_REFRACTION_H
+#define RAYTRACER_REFRACTION_H
+
+#include "MeshFunction.h"
+#include "LaserRay.h"
+#include "Gradient.h"
+#include "CollisionalFrequency.h"
 
 namespace raytracer {
-    SnellsLaw::SnellsLaw(
-            const MeshFunction &density,
-            const MeshFunction &temperature,
-            const MeshFunction &ionization,
-            const Gradient &gradientCalculator,
-            const CollisionalFrequency &collisionalFrequencyCalculator,
-            Marker* reflectedMarker
-    ) :
-            density(density),
-            temperature(temperature),
-            ionization(ionization),
-            gradientCalculator(gradientCalculator),
-            collisionalFrequencyCalculator(collisionalFrequencyCalculator),
-            reflectedMarker(reflectedMarker)
-    {}
+    /**
+     * \addtogroup directionFinders
+     * @{
+     */
 
-    Vector SnellsLaw::operator()(
-            const PointOnFace &pointOnFace,
-            const Vector &previousDirection,
-            const Element &previousElement,
-            const Element &nextElement,
-            const LaserRay &laserRay
-    ) {
-        const auto gradient = gradientCalculator.get(pointOnFace, previousElement, nextElement);
-        if (gradient.getNorm() == 0) return previousDirection;
-
-        const auto rho1 = Density{density.getValue(previousElement)};
-        const auto rho2 = Density{density.getValue(nextElement)};
-
-        const auto T1 = Temperature{temperature.getValue(previousElement)};
-        const auto T2 = Temperature{temperature.getValue(nextElement)};
-
-        const auto Z1 = ionization.getValue(previousElement);
-        const auto Z2 = ionization.getValue(nextElement);
-
-        const auto nu_ei_1 = collisionalFrequencyCalculator.get(rho1, T1, laserRay.wavelength,
-                                                                Z1);
-        const auto nu_ei_2 = collisionalFrequencyCalculator.get(rho2, T2, laserRay.wavelength,
-                                                                Z2);
-
-        const double n1 = laserRay.getRefractiveIndex(rho1, nu_ei_1);
-        const double n2 = laserRay.getRefractiveIndex(rho2, nu_ei_2);
-
-        const auto l = 1 / previousDirection.getNorm() * previousDirection;
-        auto n = 1 / gradient.getNorm() * gradient;
-        auto c = (-1) * n * l;
-        if (c < 0) {
-            c = -c;
-            n = (-1) * n;
+    /**
+     * Functor that returns always the previousDirection.
+     */
+    struct ContinueStraight {
+        /**
+         * Function to be used in Laser::generateIntersections() as findDirection. It returns the previousDirection
+         * given, effectively continuing in a straight line.
+         * @param previousDirection
+         * @return previousDirection
+         */
+        Vector operator()(
+                const PointOnFace &,
+                const Vector &previousDirection,
+                const Element &,
+                const Element &,
+                const LaserRay &
+        ) {
+            return previousDirection;
         }
-        const double r = n1 / n2;
+    };
 
-        auto root = 1 - r * r * (1 - c * c);
-        if (root > 0) {
-            return r * l + (r * c - sqrt(root)) * n;
-        } else {//Reflection
-            if (gradient*previousDirection < 0){//Against gradient
-                return previousDirection;
-            }
-            else {
-                if(reflectedMarker) reflectedMarker->mark(previousElement);
-                return l + 2 * c * n;
-            }
-        }
-    }
+/**
+ * Functor that finds new direction base on the Snells's law.
+ */
+    struct SnellsLaw {
+        /**
+         * Construct the SnellsLaw using the density, temperature and ionization function references.
+         * Provide gradientCalculator calculate the plane of rarefaction. Provide collisionalFrequencyCalculator
+         * to evaluate the index of rarefaction.
+         * Density, temperature and ionization values are expected to change.
+         * @param density
+         * @param temperature
+         * @param ionization
+         * @param gradientCalculator
+         * @param collisionalFrequencyCalculator
+         */
+        explicit SnellsLaw(
+                const MeshFunction &density,
+                const MeshFunction &temperature,
+                const MeshFunction &ionization,
+                const Gradient &gradientCalculator,
+                const CollisionalFrequency &collisionalFrequencyCalculator,
+                Marker* reflectedMarker = nullptr
+        );
+
+
+        /**
+         * Function to be used in Laser::generateIntersections() as findDirection.
+         * Finds new direction based on Snells law between the two Element, density (provides an index of rarefaction)
+         * and gradient (provides edge direction). Could throw error no intersection found!
+         *
+         * @param pointOnFace
+         * @param previousDirection
+         * @param previousElement
+         * @param nextElement
+         * @param laserRay
+         * @return new direction based on Snells law.
+         */
+        Vector operator()(
+                const PointOnFace &pointOnFace,
+                const Vector &previousDirection,
+                const Element &previousElement,
+                const Element &nextElement,
+                const LaserRay &laserRay
+        );
+
+    private:
+        const MeshFunction &density;
+        const MeshFunction &temperature;
+        const MeshFunction &ionization;
+        const Gradient &gradientCalculator;
+        const CollisionalFrequency &collisionalFrequencyCalculator;
+        Marker* reflectedMarker;
+    };
+
+    /**
+     * @}
+     */
 }
+
+#endif //RAYTRACER_REFRACTION_H
--- src/physics/Refraction.cpp
+++ src/physics/Refraction.cpp
@@ -2,68 +2,69 @@
 #include "Refraction.h"
 
 namespace raytracer {
-        SnellsLaw::SnellsLaw(
-                const MeshFunction &density,
-                const MeshFunction &temperature,
-                const MeshFunction &ionization,
-                const Gradient &gradientCalculator,
-                const CollisionalFrequency &collisionalFrequencyCalculator,
-                Marker* reflectedMarker
-        ) :
-                density(density),
-                temperature(temperature),
-                ionization(ionization),
-                gradientCalculator(gradientCalculator),
-                collisionalFrequencyCalculator(collisionalFrequencyCalculator),
-                reflectedMarker(reflectedMarker)
-                {}
+    SnellsLaw::SnellsLaw(
+            const MeshFunction &density,
+            const MeshFunction &temperature,
+            const MeshFunction &ionization,
+            const Gradient &gradientCalculator,
+            const CollisionalFrequency &collisionalFrequencyCalculator,
+            Marker* reflectedMarker
+    ) :
+            density(density),
+            temperature(temperature),
+            ionization(ionization),
+            gradientCalculator(gradientCalculator),
+            collisionalFrequencyCalculator(collisionalFrequencyCalculator),
+            reflectedMarker(reflectedMarker)
+    {}
 
-        Vector SnellsLaw::operator()(
-                const PointOnFace &pointOnFace,
-                const Vector &previousDirection,
-                const Element &previousElement,
-                const Element &nextElement,
-                const LaserRay &laserRay
-        ) {
-            const auto rho1 = Density{density.getValue(previousElement)};
-            const auto rho2 = Density{density.getValue(nextElement)};
+    Vector SnellsLaw::operator()(
+            const PointOnFace &pointOnFace,
+            const Vector &previousDirection,
+            const Element &previousElement,
+            const Element &nextElement,
+            const LaserRay &laserRay
+    ) {
+        const auto gradient = gradientCalculator.get(pointOnFace, previousElement, nextElement);
+        if (gradient.getNorm() == 0) return previousDirection;
 
-            const auto T1 = Temperature{temperature.getValue(previousElement)};
-            const auto T2 = Temperature{temperature.getValue(nextElement)};
+        const auto rho1 = Density{density.getValue(previousElement)};
+        const auto rho2 = Density{density.getValue(nextElement)};
 
-            const auto Z1 = ionization.getValue(previousElement);
-            const auto Z2 = ionization.getValue(nextElement);
+        const auto T1 = Temperature{temperature.getValue(previousElement)};
+        const auto T2 = Temperature{temperature.getValue(nextElement)};
 
-            const auto nu_ei_1 = collisionalFrequencyCalculator.get(rho1, T1, laserRay.wavelength,
-                                                                    Z1);
-            const auto nu_ei_2 = collisionalFrequencyCalculator.get(rho2, T2, laserRay.wavelength,
-                                                                    Z2);
+        const auto Z1 = ionization.getValue(previousElement);
+        const auto Z2 = ionization.getValue(nextElement);
 
-            const double n1 = laserRay.getRefractiveIndex(rho1, nu_ei_1);
-            const double n2 = laserRay.getRefractiveIndex(rho2, nu_ei_2);
+        const auto nu_ei_1 = collisionalFrequencyCalculator.get(rho1, T1, laserRay.wavelength,
+                                                                Z1);
+        const auto nu_ei_2 = collisionalFrequencyCalculator.get(rho2, T2, laserRay.wavelength,
+                                                                Z2);
 
-            const auto gradient = gradientCalculator.get(pointOnFace, previousElement, nextElement);
+        const double n1 = laserRay.getRefractiveIndex(rho1, nu_ei_1);
+        const double n2 = laserRay.getRefractiveIndex(rho2, nu_ei_2);
 
-            const auto l = 1 / previousDirection.getNorm() * previousDirection;
-            auto n = 1 / gradient.getNorm() * gradient;
-            auto c = (-1) * n * l;
-            if (c < 0) {
-                c = -c;
-                n = (-1) * n;
+        const auto l = 1 / previousDirection.getNorm() * previousDirection;
+        auto n = 1 / gradient.getNorm() * gradient;
+        auto c = (-1) * n * l;
+        if (c < 0) {
+            c = -c;
+            n = (-1) * n;
+        }
+        const double r = n1 / n2;
+
+        auto root = 1 - r * r * (1 - c * c);
+        if (root > 0) {
+            return r * l + (r * c - sqrt(root)) * n;
+        } else {//Reflection
+            if (gradient*previousDirection < 0){//Against gradient
+                return previousDirection;
             }
-            const double r = n1 / n2;
-
-            auto root = 1 - r * r * (1 - c * c);
-            if (root > 0) {
-                return r * l + (r * c - sqrt(root)) * n;
-            } else {//Reflection
-                if (gradient*previousDirection < 0){//Against gradient
-                    return previousDirection;
-                }
-                else {
-                    if(reflectedMarker) reflectedMarker->mark(previousElement);
-                    return l + 2 * c * n;
-                }
+            else {
+                if(reflectedMarker) reflectedMarker->mark(previousElement);
+                return l + 2 * c * n;
             }
         }
+    }
 }
